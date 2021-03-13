802a5074c7bc84b39f2d674e5795665b6b62462f
Markus Knapp <markus_knapp@Markuss-MacBook-Pro.local>
2021-03-13 16:46:58 +0100

    View und Objekte verknüpft und ClickListener erstellt

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button2);
+
+        textView = findViewById(R.id.textView);
+        textView.setText("Answer from Server");
+        editText = findViewById(R.id.editTextNumber);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
